6042fe41894e636eaf458711935a0345b731548d
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-11 16:26:42 -0600

    Merge conflicts fix

--- hrapp/views/connection.py
+++ hrapp/views/connection.py
@@ -1,6 +1,2 @@
 class Connection:
-<<<<<<< HEAD
-    db_path = "/Users/josephwalker/workspace/PYTHON/bangazon-workforce-mgt-virtual-magicians/db.sqlite3"
-=======
     db_path = "/Users/clb/workspace/python/bangazon-workforce-mgt-virtual-magicians/db.sqlite3"
->>>>>>> master
