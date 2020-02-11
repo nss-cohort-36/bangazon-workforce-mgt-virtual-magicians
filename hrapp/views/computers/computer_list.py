f3dc7f284d3192151893b8e012aa3e1e2c1a9bbd
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-11 15:18:49 -0600

    New PR

--- hrapp/views/computers/computer_list.py
+++ hrapp/views/computers/computer_list.py
@@ -1,6 +1,7 @@
 import sqlite3
 from django.shortcuts import render
 from models import Computer
+from ..connection import Connection
 
 
 def computer_list(request):
@@ -20,3 +21,18 @@ def computer_list(request):
             all_computers = []
             dataset = db_cursor.fetchall()
 
+            for row in dataset:
+                computer = Computer()
+                computer.id = row['id']
+                computer.make = make['make']
+                computer.purchase_date = purchase_date['purchase_date']
+                computer.decomission_date = decomission_date['decomission_date']
+
+                all_computers.append(computer)
+
+        template = 'computers/computer_list.html'
+        context = {
+            'all_computers': all_computers
+        }
+
+        return render(request, template, context)
