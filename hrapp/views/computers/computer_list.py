d58d931faa91671e8b5304e66e966638c02f46a1
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-11 14:09:05 -0600

    Starting computer_list code

---
+++ hrapp/views/computers/computer_list.py
@@ -0,0 +1,21 @@
+import sqlite3
+from django.shortcuts import render
+
+
+def computer_list(request):
+    if request.method == 'GET'
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+                b.id,
+                b.title,
+                b.isbn,
+                b.author,
+                b.year_published,
+                b.librarian_id,
+                b.location_id
+            from hrapp_computer c
+            """)
