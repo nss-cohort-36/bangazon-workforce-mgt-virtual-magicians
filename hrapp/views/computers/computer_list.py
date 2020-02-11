63f632c0fdc53b7f983e0546fc8a72c054802232
Cassie Boyd <cassandra.bacon@gmail.com>
2020-02-11 15:48:11 -0600

    Adding path for computer_list

--- hrapp/views/computers/computer_list.py
+++ hrapp/views/computers/computer_list.py
@@ -1,11 +1,11 @@
 import sqlite3
 from django.shortcuts import render
-from models import Computer
+from .models import Computer
 from ..connection import Connection
 
 
 def computer_list(request):
-    if request.method == 'GET'
+    if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
