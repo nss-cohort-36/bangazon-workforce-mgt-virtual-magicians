baa0d00669d04cc596f18e9d117dda65930b6fdc
joseph walker <josephwalker504@gmail.com>
2020-02-12 14:34:01 -0600

    new pull request

--- hrapp/views/departments/department_list.py
+++ hrapp/views/departments/department_list.py
@@ -5,41 +5,40 @@ from ..connection import Connection
 from hrapp.models.employee import Employee
 
 
-
 def department_list(request):
     if request.method == 'GET':
-        with sqlite3.connect(Connection.db_path) as conn:            
+        with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
 
-
             db_cursor.execute("""
-            select
-                d.id,
-                d.dept_name,
-                d.dept_budget
-                d.employee_id
-                e.first_name
+            SELECT 
+                d.id, 
+                d.dept_name, 
+                d.dept_budget, 
+                e.id, 
+                e.department_id, 
+                e.first_name, 
                 e.last_name
-                e.id
-            from hrapp_department d
-            JOIN hrapp_employee
-            where d.employee_id = e.id;
+            FROM hrapp_department d
+            INNER JOIN hrapp_employee e
+            ON d.id =e.department_id;
             """)
-
             all_departments = []
             dataset = db_cursor.fetchall()
+
 
             for row in dataset:
                 department = Department()
                 department.id = row['id']
                 department.dept_name = row['dept_name']
                 department.dept_budget = row['dept_budget']
+                department.department_id = row['department_id']
                 all_departments.append(department)
-              
+
     template = 'department/department_list.html'
     context = {
-        'departments' : all_departments
+        'departments': all_departments
     }
 
     return render(request, template, context)
