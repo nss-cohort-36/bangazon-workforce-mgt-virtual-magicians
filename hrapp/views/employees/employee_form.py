0f1d68aeeea6b170dfa028a924ec7a96079e5826
Julian Garcia <garcia.j.julian@gmail.com>
2020-02-13 09:12:16 -0600

    added the feature to where you can now add an employee to a department

--- hrapp/views/employees/employee_form.py
+++ hrapp/views/employees/employee_form.py
@@ -8,9 +8,26 @@ from hrapp.models import Department
 from ..connection import Connection
 
 
+def get_departments():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            d.id,
+            d.dept_name
+        from hrapp_department d
+        """)
+
+        return db_cursor.fetchall()
+
 def employee_form(request):
     if request.method == 'GET':
+        department = get_departments()
         template = 'employees/employee_form.html'
-        context = {}
+        context = {
+            'all_departments': department
+        }
 
         return render(request, template, context)
--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -60,11 +60,12 @@ def employee_list(request):
             INSERT INTO hrapp_employee
             (
                 first_name, last_name, start_date,
-                is_supervisor
+                is_supervisor, department_id
             )
-            VALUES (?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?)
             """,
             (form_data['first_name'], form_data['last_name'],
-                form_data['start_date'], form_data['is_supervisor']))
+                form_data['start_date'], form_data['is_supervisor'], 
+                    form_data['department']))
 
-        return redirect(reverse('hrapp:employees'))
+        return redirect(reverse('hrapp:employee_list'))
